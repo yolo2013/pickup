4f01c63d49028e337296f7a2cfe8565ea2c97637
yolo2013 <i_vagrancy@163.com>
2017-05-19 14:24:07 +0800

    补提交

--- fanfou-timeline/config.js
+++ fanfou-timeline/config.js
@@ -4,5 +4,5 @@ module.exports = {
   // 要抓取的用户ID
   userId: '~RLhcIDBjZAM',
   // 你自己的cookie
-  cookie: '__utmt=1; u=%7E_tHGUPVzIXY; uuid=e41d08e3b8129bc6aa06.1495160263.3; PHPSESSID=23o940lcb6kjq3aml8lmrgs0n7; m=i_vagrancy%40163.com; __utma=208515845.867024362.1495160268.1495164494.1495172770.3; __utmb=208515845.5.10.1495172770; __utmc=208515845; __utmz=208515845.1495160268.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=208515845.other'
+  cookie: ''
 }
